c3854472971bde42a3cf0c3eebeedd55d9e438d6
Keziah Plattner <keziah@stanford.edu>
2012-11-08 17:57:24 -0800

    edit is (maybe) working

--- editinfo.php
+++ editinfo.php
@@ -1,10 +1,11 @@
 <?php session_start();
-$info=$_POST['textarea']; 
 include("config.php");
-$project=$_SESSION['activeProj'];
-$update = "UPDATE projectinfo SET info='$newinfo' WHERE projectname='$project'";
+$info=$_POST['newInfo'];
+$project=$_POST['projectname'];
+$update = "UPDATE projectinfo SET info='$info' WHERE projectname='$project'";
 mysql_query($update);
 
-//echo('<meta http-equiv="REFRESH" content="0; URL=project.php">');
 
 ?>
+<meta http-equiv="REFRESH" content="0; URL=project.php?projectname=<?php echo "".$project."" ?>">
+
