7631f002be5ddf455a7a5b5906f322a526eb0e28
lilith <lilithwu@stanford.edu>
2012-11-14 16:12:50 -0800

    temporarily put back lyrics popup code on project page

--- project.php
+++ project.php
@@ -120,7 +120,25 @@ $oldInfo =$row['info'];
 </div><!-- /page two -->
 
 
+	<a href="#editlyrics" data-role="button" data-rel="dialog" data-transition="pop">Edit Lyrics</a>
+	
+	<div data-role="page" class="ui-dialog ui-page ui-body-c" id="editlyrics" data-url="editlyrics" role="dialog">
+<div data-role="header">
+<h1>Edit Lyrics</h1>
+</div>
+<div data-role="content">
 
+<form action="editlyrics.php" id="edit2" method="POST" data-ajax="false">
+<div data-role="fieldcontain">
+	<textarea cols="40" rows="8" name="lyrics" id="lyrics"><?php echo "".$row['lyrics']."" ?> </textarea>
+	<input type="hidden" name="projectname" value="<?php echo "$project" ?>"/><p>
+	<input type="submit" value="Submit"/>
+</div>
+</form>
+
+</div><!-- /content -->
+</div><!-- /page three -->
+	
 
 
 
